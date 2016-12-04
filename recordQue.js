c5d81f30e03787c41895a2c01ca5c127ced21118
Anthony Aragues <anthony.aragues@gmail.com>
2016-12-04 16:27:56 -0500

    a little cleanup now that the directive works

--- recordQue.js
+++ recordQue.js
@@ -1,6 +1,5 @@
 angular.module("ngRecordQue", []).directive('recordQue',function(){
 	'use strict';
-	console.log('return the prime directive');
 	return {
 		 restrict:'E'
 		,replace: true
@@ -8,7 +7,11 @@ angular.module("ngRecordQue", []).directive('recordQue',function(){
 		,scope:{ d:'=' }
 		,template: '<table class="recordQue"><tr><td id="nw" class="{{::d.nw}}"></td><td id="ne" class="{{::d.ne}}"></td></tr><tr><td id="sw" class="{{::d.sw}}"></td><td id="se" class="{{::d.se}}"></td></tr></table>'
 		,controller:function($scope){
-			console.log('data: ',$scope.d);	
+			//add a little flexibility can define classes as space separated or an array, becasue arrays are often easier to work with
+			if($scope.d.nw.constructor === Array){ $scope.d.nw=$scope.d.nw.join(' '); }
+			if($scope.d.ne.constructor === Array){ $scope.d.ne=$scope.d.ne.join(' '); }
+			if($scope.d.sw.constructor === Array){ $scope.d.sw=$scope.d.sw.join(' '); }
+			if($scope.d.se.constructor === Array){ $scope.d.se=$scope.d.se.join(' '); }
 		}
 	};
 });
